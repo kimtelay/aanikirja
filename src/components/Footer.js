cf323a3213593239729e5f31b50c5ae32d8963fa
kimtel <alavus@gmail.com>
2019-09-08 16:19:36 +0300

    Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logo from '../img/logo2.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,7 +13,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src={logo2}
             alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
